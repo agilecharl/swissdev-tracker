d97ce86ad9746195c7b6f77ca35e722ff5293610
agilecharl <agilecharl@gmail.com>
2025-07-29 23:43:48 +0200

    feat(data): implement RESTful API functions for data management

--- libs/data/src/lib/rest.tsx
+++ libs/data/src/lib/rest.tsx
@@ -1,29 +1,76 @@
-import { useEffect, useState } from 'react';
+const apiUrl = process.env.API_URL ?? '';
 
-export function Rest() {
-  return (
-    <div>
-      <h1>Welcome to Rest!</h1>
-    </div>
-  );
-}
-export function useFetchData(endpoint: string) {
-  const [data, setData] = useState<any>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<any>(null);
+export const getRecords = (url: string, params: any) => {
+  let localApiUrl = `${apiUrl}/${url}`;
+  localApiUrl = `${localApiUrl}`;
 
-  useEffect(() => {
-    setLoading(true);
-    fetch(`http://localhost:3001/${endpoint}`)
-      .then((res) => {
-        if (!res.ok) throw new Error('Network response was not ok');
-        return res.json();
-      })
-      .then(setData)
-      .catch(setError)
-      .finally(() => setLoading(false));
-  }, [endpoint]);
+  return fetch(localApiUrl, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(async (data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error('Error in getRecords:', error);
+      throw error;
+    });
+};
 
-  return { data, loading, error };
-}
-export default Rest;
+export const insertRecord = (url: string, content: any) => {
+  let localApiUrl = `${apiUrl}/${url}`;
+  localApiUrl = `${localApiUrl}`;
+
+  return fetch(localApiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(content),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+};
+
+export const updateRecord = (url: string, content: any) => {
+  let localApiUrl = `${apiUrl}/${url}`;
+  localApiUrl = `${localApiUrl}`;
+
+  return fetch(localApiUrl, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(content),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+};
+
+export const deleteRecord = (url: string, params: any) => {
+  let localApiUrl = `${apiUrl}/${url}`;
+  localApiUrl = `${localApiUrl}`;
+
+  return fetch(localApiUrl, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+};

70ac17604f7dbc47bc7d18f2a225de1e96a3db60
agilecharl <agilecharl@gmail.com>
2025-10-10 18:16:09 +0200

    fix: correct package declaration in HomeController.java

--- apps/backend/src/main/java/com/swissdev/tracker/controller/HomeController.java
+++ apps/backend/src/main/java/com/swissdev/tracker/controller/HomeController.java
@@ -1,4 +1,4 @@
-package main.java.com.swissdev.tracker.controller;
+package com.swissdev.tracker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
